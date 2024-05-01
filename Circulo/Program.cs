d83b20e53194eb9f4965ddca2047e93e32188e9c
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-05-01 08:58:12 -0500

    Ejercice two

---
+++ Circulo/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var radio = 0d;
+//Se declara con 0d que indica que es double
+
+var resultado = 0d;
+
+Console.WriteLine("Bienvenido, calculemos circulos");
+//Pi * radio * radio
+
+Console.WriteLine("Ingresa el valor del radio");
+radio = Convert.ToDouble(Console.ReadLine());
+
+resultado = Math.PI * radio * radio;
+
+Console.WriteLine("El area de tu circulo es: " + resultado);
