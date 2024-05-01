b72491410c1b0997821b9ace290792c6fa6690f0
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-04-30 22:31:20 -0500

    Init lenguage C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//PROGRAMA QUE CALCULA EL AREA DE UN RECTANGULO
+
+//Declaracion de una variable tipo entero
+
+int ladoA;
+int ladoB;
+int resultado;
+
+Console.WriteLine("Calcula el area del rectangulo");
+Console.WriteLine("Ingrese el valor del lado A");
+
+ladoA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el valor del lado B");
+ladoB = Convert.ToInt32(Console.ReadLine());
+
+resultado = ladoA * ladoB;
+
+Console.WriteLine("El resultado es: " + resultado);
