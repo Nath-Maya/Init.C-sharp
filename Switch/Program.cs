af56c9c9e0dd6829ac1ed4ab5e59e930dc8abf0d
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-05-02 18:40:15 -0500

    Refactorization

--- Switch/Program.cs
+++ Switch/Program.cs
@@ -1,17 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Drawing;
+﻿using System.Drawing;
 
 Console.WriteLine("Hello, World!");
 int totalJugador = 0;
 int totalDealer = 0;
 int num = 0;
 string message = "";
+string controlOtraCarta = "";
 string switchControl = "menu";
+
+System.Random random = new System.Random();
+
 //Blackjack, Juntar 21 pidiendo, en casa de pasarte de 21 pierdes.
 //cartas o en caso de tener menos
 //de 21 igual tener mayor puntuación que el dealer
-while (true)
-{
+while (true){
+    totalJugador = 0;
+    totalDealer = 0;
+
     switch (switchControl)
     {
         case "menu":
@@ -22,15 +27,20 @@ while (true)
         case "21":
                 do
             {
-                System.Random random = new System.Random();
                 num = random.Next(1, 12);
                 totalJugador = totalJugador + num;
                 Console.WriteLine("Toma tu carta, jugador,");
                 Console.WriteLine($"Te salió el número: { num} ");
                 Console.WriteLine("¿Deseas otra carta ?");
-            } while (Console.ReadLine() == "Si" ||
-            Console.ReadLine() == "si"
-            || Console.ReadLine() == "yes");
+                controlOtraCarta = Console.ReadLine();
+
+            } while (controlOtraCarta == "Si" ||
+            controlOtraCarta == "si"
+            || controlOtraCarta == "yes");
+
+            totalDealer = random.Next(14, 23);
+            Console.WriteLine($"El dealer tiene {totalDealer}");
+
             if (totalJugador > totalDealer && totalJugador < 22)
             {
                 message = "Venciste al dealer, felicidades";
