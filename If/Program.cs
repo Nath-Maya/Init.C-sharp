cd64d49f4295c9d12f180465932f21fd50f53536
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-05-01 21:19:14 -0500

    Conditional IF

---
+++ If/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int totalJugador = 20;
+int totalDealer = 15;
+string message = "";
+
+if ( totalJugador < totalDealer )
+{
+   message = "Venciste al dealer, felicidades";
+}
+else if (totalJugador <= totalDealer)
+{
+   message = "Perdiste, lo siento";
+}
+else
+{
+   message = "Condición no valida";
+}
+
+Console.WriteLine(message);
+
